03497502508be498d94dbfc65dc326b7cefbeb93
Horleryheancarh <oladipoolayinka7@gmail.com>
2021-08-30 14:19:51 +0100

    misc

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ mongoose.connect(process.env.DB_URI, {
 // START SERVER
 const start = async () => {
     try {
-        await fastify.listen(process.env.PORT)
+        await fastify.listen(process.env.PORT || 3000)
     } catch (error) {
         fastify.log.error(error)
     }
